4594f2756f2d95b7ed543ff7dc2eb7db04a89047
José Umberto <joseumbertomoreira@gmail.com>
2024-09-09 21:44:10 -0300

    A resposta do exercicio 1

--- Ex1_media_dois_valores/media_dois_valores.c
+++ Ex1_media_dois_valores/media_dois_valores.c
@@ -3,7 +3,14 @@
 
 int main(){
 
-    printf("Ola Mundo!");
+    float nota_a, nota_b;
+    printf("Leia a nota do aluno 1: ");
+    scanf("%f", &nota_a);
+
+    printf("Leia a nota do aluno 2: ");
+    scanf("%f", &nota_b);
+
+    printf("A média das notas do aluno1 e do aluno2 = %.2f\n", (nota_a+nota_b)/2);
 
     return 0;
 }
